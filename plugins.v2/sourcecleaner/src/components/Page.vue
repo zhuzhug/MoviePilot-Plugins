35fddb2b8fabca3a24aad7d057a0363c0f8cc282
●.丨潴潴↘G <37032410+zhuzhug@users.noreply.github.com>
2026-07-29 14:06:22 +0800

    v2.0.0 Vue前端重构

---
+++ plugins.v2/sourcecleaner/src/components/Page.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card variant="flat">
+    <v-card-text>
+      <AppPage :api="api" :pluginId="pluginId" />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import AppPage from './AppPage.vue';
+export default {
+  name: 'Page',
+  components: { AppPage },
+  props: {
+    api: { type: Object, required: true },
+    pluginId: { type: String, required: true },
+  },
+};
+</script>
